5b807ded0b88153e94c9b3621e4dce9265e013f7
Md Humair Raza <112873688+mdhumairraza@users.noreply.github.com>
2023-09-10 00:21:21 +0530

    Update problem1.java

--- Q1/problem1.java
+++ Q1/problem1.java
@@ -1,1 +1,25 @@
-ok
+/* 1. Write a function to convert a decimal number string into binary string.
+For example, string A contains “123” which is decimal value 123. Convert
+this string into binary string. */
+public class Main{
+    public static String decimalToBinary(String dec){
+        int num=Integer.parseInt(dec);
+        if(num==0){
+            return "0";
+        }else{
+            StringBuilder str=new StringBuilder();
+            while(num > 0){
+                int x=num%2;
+                str.insert(0,x);
+                num=num/2;
+            }
+            return str.toString();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String res=decimalToBinary(str);
+        System.out.println(res);
+    }
+}
